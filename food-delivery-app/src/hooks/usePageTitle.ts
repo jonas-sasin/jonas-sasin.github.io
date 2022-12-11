1c89008e0dc91258e7302e8c208a969305d3ecf1
Jonas Sasin <xsasin1@fi.muni.cz>
2022-12-11 18:17:54 +0100

    delete sources

--- food-delivery-app/src/hooks/usePageTitle.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-const usePageTitle = (title: string) => {
-	useEffect(() => {
-		document.title = `${title} | FoodDelivery`;
-	}, [title]);
-};
-
-export default usePageTitle;
